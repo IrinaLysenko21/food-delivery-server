4d7927c9554d500fca920e68ce8a868e42743e61
Irina Lysenko <irina.lysenko21@gmail.com>
2019-11-28 12:39:02 +0200

    getAllProducts function restored

--- src/products/controllers/get_all_products.js
+++ src/products/controllers/get_all_products.js
@@ -1,8 +1,9 @@
 const Product = require('../product_schema');
+const Ingredient = require('../../ingredients/ingredient_schema');
 
 const getAllProducts = async (request, response) => {
   try {
-    const products = await Product.find();
+    const products = await Product.find().populate('ingredients');
 
     response.status(200).json({ status: "success", products });
   } catch (err) {
